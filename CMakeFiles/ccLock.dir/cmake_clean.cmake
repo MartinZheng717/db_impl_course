cd5925af1768b223734a66efdd67283858cfceb0
Martin717 <zhengzhiwei717@163.com>
2024-05-31 05:08:27 +0800

    2pl

---
+++ CMakeFiles/ccLock.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ccLock.dir/cc_lock.cpp.o"
+  "CMakeFiles/ccLock.dir/cc_lock.cpp.o.d"
+  "CMakeFiles/ccLock.dir/main.cpp.o"
+  "CMakeFiles/ccLock.dir/main.cpp.o.d"
+  "CMakeFiles/ccLock.dir/structure.cpp.o"
+  "CMakeFiles/ccLock.dir/structure.cpp.o.d"
+  "ccLock"
+  "ccLock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ccLock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ccLock.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ccLock.
+# This may be replaced when dependencies are built.
